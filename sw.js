0156b5679fe763aef0936c2169cf3e730c05541b
theanh1706 <50204439+theanh1706@users.noreply.github.com>
2026-04-06 19:25:49 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,7 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('salary-app').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ]);
-    })
-  );
+self.addEventListener("install", e => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener("activate", e => {
+  self.clients.claim();
 });
