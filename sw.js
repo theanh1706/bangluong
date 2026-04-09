d236fdfcd38abdbc12d73dda722bedc85051d174
theanh1706 <50204439+theanh1706@users.noreply.github.com>
2026-04-09 17:20:48 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,29 @@
-self.addEventListener("install", e => {
+const CACHE_NAME = 'salary-pwa-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './sw.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
   self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const cloned = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, cloned));
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
 });
